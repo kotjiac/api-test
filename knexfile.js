a36598a847d30ae2d696e0e774076ebaf223ed5b
Ronaldo Teixeira <ronaldoalvesteixeira@gmail.com>
2021-11-18 00:55:07 -0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,8 @@ module.exports = {
       database: process.env.DB_NAME,
       user:     process.env.DB_USER,
       password: process.env.DB_PASS,
-      charset: 'utf8'
+      charset: 'utf8',
+      ssl: true
     },
     pool: {
       min: 2,
