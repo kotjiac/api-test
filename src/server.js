bf986981e944fe64b6132ee0d63fe239d88175f9
Ronaldo Teixeira <ronaldoalvesteixeira@gmail.com>
2021-11-25 11:55:31 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,12 +5,14 @@ const cors = require('cors');
 const routes = require('./routes');
 const PORT = process.env.PORT || 3333;
 const HOST = process.env.HOST || '0.0.0.0'
+var AWSXRay = require('aws-xray-sdk');
 
 const app = express();
 
 //app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
+app.use(AWSXRay.express.openSegment('AWSXRay: API-Test'));
 app.use(routes);
 
 app.listen(PORT, HOST, () => {
